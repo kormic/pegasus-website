0ad0eb3234894790687288c0bfef30827fbc5c44
kormic <michayc@yahoo.com>
2015-01-28 19:40:50 +0200

    Uploaded all files

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(function(){
+
+    $('#myAlert').click(function(e){
+        e.preventDefault();
+        $('#successAlert').slideDown();
+    });
+
+    $('a.pop').click(function(e){
+        e.preventDefault();
+    });
+
+    $('a.pop').popover();
+
+    $('[rel="tooltip"]').tooltip();
+
+    $('[rel="tooltip"]').click(function(e){
+        e.preventDefault();
+    });
+});
